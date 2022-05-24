28f54cc1bee5feba9f535893d0b2ffa8c61e20bd
Hardik <hardikdharmik94@gmail.com>
2022-05-24 16:40:25 +0530

    Model Integrated

--- BACKEND_API/views.py
+++ BACKEND_API/views.py
@@ -1,8 +1,31 @@
 from django.http import JsonResponse
 
+import json
+import requests
+
+API_TOKEN = "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"
+
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+API_URL = "https://api-inference.huggingface.co/models/hd94/roberta-hindi"
+
+def query(payload):
+    data = json.dumps(payload)
+    response = requests.request("POST", API_URL, headers=headers, data=data)
+    return json.loads(response.content.decode("utf-8"))
+
+
 def index(request):
     if request.method == 'POST':
         context = request.POST.get('context')
         question = request.POST.get('question')
-        return JsonResponse({'context' : context, 'question' : question, 'answer' : 'This is answer'})
+
+        data = query({
+                "inputs": {
+                    "question": question,
+                    "context": context,
+                }
+            }
+        )
+
+        return JsonResponse({'data' : data})
     return JsonResponse({'error' : 'ERROR!!!'})
